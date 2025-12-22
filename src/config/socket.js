89788cf73f9444bb00a74dff9e2c2fbaa1a90af2
Ashutosh Sharma <ashup8998@gmail.com>
2025-12-22 23:55:33 +0530

    allowd all origin

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,20 +1,27 @@
-const allowedOrigins = process.env.ALLOWED_ORIGINS
-  ?.split(",")
-  .map(o => o.trim());
+// const allowedOrigins = process.env.ALLOWED_ORIGINS
+//   ?.split(",")
+//   .map(o => o.trim());
 
+// export const socketConfig = {
+//   cors: {
+//     origin: (origin, callback) => {
+//       // allow server-to-server or Postman (no origin)
+//       if (!origin) return callback(null, true);
+
+//       if (allowedOrigins.includes(origin)) {
+//         return callback(null, true);
+//       }
+
+//       return callback(new Error("Not allowed by CORS"));
+//     },
+//     credentials: true,
+//     methods: ["GET", "POST"]
+//   }
+// };
 export const socketConfig = {
   cors: {
-    origin: (origin, callback) => {
-      // allow server-to-server or Postman (no origin)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
+    origin: "*",
     credentials: true,
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 };
